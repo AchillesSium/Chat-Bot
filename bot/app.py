66c87704a7925189da0efc4e731bb9177cbd29af
Juho Eerola <juho.eerola@tuni.fi>
2020-10-07 16:44:24 +0300

    Implement initial enrolment
    Options at the moment for consideration and comments:
    - slash command `/enrol <employee_id>`
    - direct message to the bot `enrol[l*] <employee_id>`

--- bot/app.py
+++ bot/app.py
@@ -5,8 +5,11 @@ from slack.signature import SignatureVerifier
 from slackeventsapi import SlackEventAdapter
 
 import json
+import re
 
 from dotenv import dotenv_values, find_dotenv
+
+from .bot import Bot
 
 # Get the tokens from .env file (.env.sample in version control)
 ENV = dotenv_values(find_dotenv())
@@ -19,69 +22,58 @@ app = Flask(__name__)
 slack_client = WebClient(SLACK_API_TOKEN)
 slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, "/slack/events", app)
 
+bot = Bot()
+
 signature_verifier = SignatureVerifier(SLACK_SIGNING_SECRET)
 
 
-@app.route("/slack/events/interact", methods=["POST"])
-def slack_app():
+@app.route("/slack/commands", methods=["POST"])
+def slash_commands():
     if not signature_verifier.is_valid_request(request.get_data(), request.headers):
         return make_response("invalid request", 403)
-    if "payload" in request.form:
-        payload = json.loads(request.form["payload"])
-        if (
-            payload["type"] == "shortcut"
-            and payload["callback_id"] == "open-modal-shortcut"
-        ):
-            # Open a new modal by a global shortcut
-            try:
-                api_response = slack_client.views_open(
-                    trigger_id=payload["trigger_id"],
-                    view={
-                        "type": "modal",
-                        "callback_id": "modal-id",
-                        "title": {"type": "plain_text", "text": "Title goes here"},
-                        "submit": {"type": "plain_text", "text": "Submit"},
-                        "close": {"type": "plain_text", "text": "Cancel"},
-                        "blocks": [
-                            {
-                                "type": "input",
-                                "label": {
-                                    "type": "plain_text",
-                                    "text": "What the field is about",
-                                },
-                                "element": {
-                                    "action_id": "a-id",
-                                    "type": "plain_text_input",
-                                },
-                            },
-                        ],
-                    },
-                )
-                return make_response("", 200)
-            except Exception as e:
-                code = e.response["error"]
-                return make_response(f"Failed to open a modal due to {code}", 200)
-        if (
-            payload["type"] == "view_submission"
-            and payload["view"]["callback_id"] == "modal-id"
-        ):
-            # Handle a data submission request from the modal
-            submitted_data = payload["view"]["state"]["values"]
-            print(submitted_data)
-            return make_response("", 200)
+    print(request.form)
+    if request.form.get("command") == "/enrol":
+        try:
+            user_id = request.form["user_id"]
+            employee_id = int(request.form["text"])
+            return bot.enrol_user(user_id, employee_id)
+        except Exception as e:
+            print(f"error: {e!r}")
+            return bot.help()
     return make_response("", 404)
+
+
+@slack_events_adapter.on("message")
+def handle_direct_message(event_data):
+    print("message", event_data)
+    message = event_data["event"]
+    if message.get("subtype") is None and message.get("channel_type") == "im":
+        command = message.get("text")
+        match = re.match(r"(?:\<@\S+\>\s+)?enrol+\s+(\d+)", command, re.IGNORECASE)
+        user = message["user"]
+        channel = message["channel"]
+        if match:
+            employee_id = int(match.group(1))
+            response = bot.enrol_user(user, employee_id)
+            response.pop("response_type", None)
+            slack_client.chat_postEphemeral(channel=channel, user=user, **response)
+            return Response(status=200)
+        response = bot.help()
+        response.pop("response_type", None)
+        slack_client.chat_postEphemeral(channel=channel, user=user, **response)
+    return Response(status=200)
 
 
 @slack_events_adapter.on("app_mention")
 def handle_message(event_data):
-    print("event", event_data)
+    print("mention", event_data)
     message = event_data["event"]
     if message.get("subtype") is None:
         command = message.get("text")
         channel_id = message["channel"]
         ts = message["ts"]
         user = message["user"]
-        message = f"Hi, <@{user}>! :tada:"
+        message = f"Hi <@{user}>! :tada:"
         slack_client.chat_postMessage(channel=channel_id, text=message)
     return Response(status=200)
 
