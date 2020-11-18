fab834e459bf4264dd063f5844a982d0ce6e3796
VelkarTheTall <vilniar@hotmail.com>
2020-11-18 15:16:04 +0200

    Removed lines which were made for printing debug messages.

--- bot/searches/find_kit.py
+++ bot/searches/find_kit.py
@@ -34,15 +34,9 @@ def find_person_by_skills(
             # Look for matches in list of requested skills.
             if person["skills"] is not None:
                 # Check to see if person has skills at all.
-                # debug_printer(requested_skill, "¤")
-                # debug_printer("This person has skills.")
                 if requested_skill in person["skills"]:
                     # A match is found.
-                    # if len(matching_person) == 0:
-                    # Person with a matching skill gets detected for the first time.
-                    # debug_printer("Requested skill found: " + requested_skill)
                     matching_person = person["employeeId"]
-                    # matching_person.append(requested_skill)
                     skillsTuple += (requested_skill,)  # Record the matched skill.
         if matching_person is not None:
             # At the end of the loop,
@@ -50,7 +44,6 @@ def find_person_by_skills(
             # append him to the list which is to be returned.
             matching_people.append((matching_person, skillsTuple,))
     # Check out the allocations for all found people.
-    # debug_printer(len(matching_people), "@")
     if len(matching_people) != 0:
         matching_people = sort_by_time(matching_people, allocations, yearWeek)
 
@@ -95,37 +88,16 @@ def add_person_in_order(people: List, newPerson: Tuple) -> List:
         wasAdded = False
         for i in range(len(people)):
             # (731, ('C',), (('2020-W47', 0.0),))   # Format of a person on the list.
-
-            # debug_printer(people[i], "II")
-            # debug_printer(str(people[i][2][0][0]), "II")
-            # debug_printer(people[i][2][0], "II")
-            # debug_printer(newPerson[2][0], "@@@")
-
             if people[i][2][0][0] >= newPerson[2][0][0]:
                 # If the new person is on the same week, or on an earlier week
-
-                # debug_printer(people[i][2][1], "II")
-                # debug_printer(newPerson[2][1], "@@@")
-
-                # if newPerson[0] == 176:
-                # debug_printer(newPerson, "SS1")
-                # debug_printer(people[i], "SS2")
-
                 if people[i][2][0][1] >= newPerson[2][0][1]:
                     # Existing entry is more preoccupied than the new person.
-                    # debug_printer("Continue", "CON")
                     people.insert(i, newPerson)
                     wasAdded = True
                     break  # After insertion, there is no need to loop through the list.
                 else:
-                    # debug_printer("Insert at " + str(i), "IN ")
-                    # debug_printer(people[i], "¤")
-                    # debug_printer(newPerson, "%")
                     continue
             else:
-                # debug_printer("Insert at " + str(i), "IN ")
-                # debug_printer(people[i], "¤")
-                # debug_printer(newPerson, "%")
                 people.insert(i, newPerson)
                 break  # After insertion, there is no need to loop through the list.
         if not wasAdded:
@@ -142,62 +114,33 @@ def sort_by_time(matching_people: List, allocationsFile: List, yearWeek: str) ->
     """
     tmpList = []
     allocList = []
-    # debug_printer(matching_people, "MM")
     for person in matching_people:
         # Cycle through all the people who were matched.
-        # debug_printer("Person loop start", "111")
         allocList = []
         if person[0] in allocationsFile:
-            # debug_printer(allocationsFile[person[0]], "ALLO")
             for allocation in allocationsFile[person[0]]:
                 # In the JSON structure of allocations, the base level units are divided
                 # into two entities: "user" and "projects"
-                # debug_printer(person, "%")
-                # debug_printer(allocation, "&")
                 if allocation["yearWeek"] >= yearWeek:
                     # Detect weeks equal or later than param yearWeek
-                    # debug_printer("got to allocation's yearWeek being lesser than param yearWeek", "Å")
-                    # debug_printer(str(allocation["yearWeek"]) + ": " + str(allocation["percentage"]), "å")
                     if allocation["percentage"] < 100:
                         # Allocation is not completely taken.
-                        # debug_printer("Got to 'percentage < 100'")
                         tmpAlloc = (
                             allocation["yearWeek"],
                             float(allocation["percentage"]) / 100,
                         )
-                        # debug_printer(tmpAlloc, "tmpA")
                         allocList = add_to_week_allocation(allocList, tmpAlloc)
-                        # debug_printer(allocList, str(person[0]) + " ")
                         if len(allocList) == 10:
                             break  # Ten entries is enough.
-            # debug_printer("allocList done", "PersonAlloDone")
         else:
             # Subject had no allocations.
-            # debug_printer(str([person[0]]) + " was not found", "*")
-            # allocList.append((int(yearWeek[-2:]), 0.0,))
             insort_left(allocList, (yearWeek, 0.0,))
-            # debug_printer(allocList, str(person[0]) + " _ ")
-            # debug_printer(person + tuple(allocList))
         # All allocations extracted for person
         if allocList:
             # Only persons for whom allocations were available.
-            # debug_printer(allocList, str(person) + " # ")
             person = person + (tuple(allocList),)
-            # debug_printer(allocList, str(person) + " $ ")
-            # debug_printer(person, "per")
             # Add in the remaining people, who are preoccupied but who are otherwise qualified.
-            # debug_printer(tmpList, "TIME")
             tmpList = add_person_in_order(tmpList, person)
-        # debug_printer("Person-loop end", "00")
-    # debug_printer(tmpList, "NED")
-    # print("NED" * 6)
-    # print("NED" * 6)
-    # for line in tmpList:
-    #     print(line)   # Debug print
-    # print("NED" * 6)
-    # print("NED" * 6)
-    # print()
-
     return tmpList  # Return the sorted list.
 
 
